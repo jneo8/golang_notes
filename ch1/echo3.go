b358d20be774316e526d83dc3bdcba93955db1e1
jneo8 <james851212@gmail.com>
2017-12-14 22:19:47 +0800

    echo3.go

---
+++ ch1/echo3.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+    "fmt"
+    "os"
+    "strings"
+)
+
+func main() {
+    // see https://golang.org/pkg/strings/#Join
+    fmt.Println(strings.Join(os.Args[1:], ","))
+}
