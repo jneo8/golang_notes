172522661739ba1fbd869ac00647e6eed709a5e2
jneo8 <james851212@gmail.com>
2018-01-06 22:52:02 +0800

    ftoc, turn 。F to 。C

---
+++ ch2/ftoc.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+    const freezingF, boilingF = 32.0, 212.0
+    fmt.Printf("%g。F = %g 。C\n", freezingF, fToC(freezingF))
+    fmt.Printf("%g。F = %g 。C\n", boilingF, fToC(boilingF))
+}
+
+
+func fToC(f float64) float64 {
+    // Turn 。F to 。C
+    return (f -32) * 5 / 9
+}
+
