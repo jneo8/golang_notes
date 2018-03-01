8f0909b88eeaad1039cbf8b075087ed7327987bc
jneo8 <james851212@gmail.com>
2018-03-01 23:26:48 +0800

    update tempconv0_sample.go

--- ch2/tempconv0_sample.go
+++ ch2/tempconv0_sample.go
@@ -3,11 +3,22 @@ package main
 import (
     "./tempconv"
     "fmt"
+    "os"
+    "strconv"
 )
 
 
 func main() {
-    fmt.Printf("123")
-    fmt.Printf("%g\n", tempconv.CToF(100))
+    for _, arg := range os.Args[1:] {
+        t, err := strconv.ParseFloat(arg, 64)
+        if err != nil {
+            fmt.Fprintf(os.Stderr, "cf: %v\n", err)
+            os.Exit(1)
+        }
+        f := tempconv.Fahrenheit(t)
+        c := tempconv.Celsius(t)
+        fmt.Printf("%s = %s, %s = %s\n",
+            f, tempconv.FToC(f), c, tempconv.CToF(c))
+    }
 }
 
